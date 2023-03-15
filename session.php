6029dd80158b36ebf5d0808d4c94a875f62b2785
buisang <bhsangxd@gmail.com>
2023-03-15 09:06:57 +0700

    add session

---
+++ session.php
@@ -0,0 +1,15 @@
+<?php
+    // khoi tao
+    if(session_id()==='')
+    session_start();
+
+    if(isset($_SESSION['counter'])){
+        //dem so lan truy cap
+        $_SESSION['counter'] +=1;
+    }else{
+        //lan dau truy cap
+        $_SESSION['counter'] = 1;
+    }
+    $msg = "<p>Ban da truy cap ". $_SESSION['counter'] ." lan vao trang </p>";
+    echo $msg;
+?>
