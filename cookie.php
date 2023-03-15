a31c93cb370f424c9b721a9943965eae0ba831a1
buisang <bhsangxd@gmail.com>
2023-03-15 08:53:49 +0700

    update cookie

--- cookie.php
+++ cookie.php
@@ -2,3 +2,11 @@
     setcookie("name","buihongsang",time()+300,"/");
     echo "Set cookie";
 ?>
+<?php
+    if(isset($_COOKIE["name"])){
+        echo "Welcome " . $_COOKIE["name"];
+    }else{
+        echo "Khong co ten";
+    }
+    
+?>
